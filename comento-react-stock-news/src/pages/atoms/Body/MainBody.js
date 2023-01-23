fe60a081b2ef8801533187a81567a18d055fdf38
wooyeonwon <dndus567@naver.com>
2023-01-24 01:31:53 +0900

    [원우연/코멘토1주차과제완료]

---
+++ comento-react-stock-news/src/pages/atoms/Body/MainBody.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../../assets/MainBody.css";
+const MainBody = () => {
+  return (
+    <div className="body">
+      <p className="body_description">메인 바디 영역입니다.</p>
+    </div>
+  );
+};
+
+export default MainBody;
